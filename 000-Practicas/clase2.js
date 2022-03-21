860514e65e80113b087d29dc76d53f19e05f9933
Arbusta <gabriel.munoz@arbusta.net>
2022-03-21 12:05:57 -0300

    se agregan ejercicios

---
+++ 000-Practicas/clase2.js
@@ -0,0 +1,65 @@
+/* let nombre = "34"; // string = texto
+
+let numero= 34; // number (operaciones matematicas ej)
+
+var booleano= true; //false
+
+const nombre4 = "Marcelo" // no se puede cambiar el valor
+
+//CamelCase
+let nombreApellido = "Juan Gabriel" // lowerCamelCase
+let NombreNombre2 = "pepe pepe" // UpperCamelCase
+
+//Snake_Case
+let apellido_nombre = " Fernandez Juan " //Snake_Case */
+
+/* let nombre = "Pedro"; */
+
+
+//operaciones Matematicas
+/* let numero1 = 21;
+let numero2= 6;
+
+let suma  = numero1 + numero2;
+let resta =numero1 - numero2;
+let multiplica=numero1 * numero2;
+let divide =numero1 / numero2;
+let modulo =numero1 % numero2;
+
+document.write (modulo); */
+
+//Math
+
+/* let num1 = 3.9;
+let num2 = 3; */
+
+/* let resultado = Math.pow(num1,num2);- //math.pow devolver un número elevado a un exponente*/
+/* let resultado = Math.round(num1) //math.round redondeará un número al número entero más cercano*/
+/* let resultado = Math.floor(num1)  */ //math.floor siempre redondeará un número al número entero más cercano hacia abajo*/
+/* let resultado = Math.ceil(num1)  *//*  math.ceil siempre redondeará un número al número entero más cercano hacia arriba */
+/* document.write (resultado); */
+
+//.lenth
+/*  let nombre = "pepe";
+
+ document.write (nombre.length); */
+
+ let nombre = prompt ("Escribe tu nombre");
+
+ alert ("Hola " + nombre);
+
+//Ejercicio (Investigar parseINT y ParseFLoat )
+
+ // Crear un programa que haga lo siguiente:
+ // pedir el nombre al usuario/a .
+ // pedir un numero al usuario/a .
+ // pedir otro numero
+
+ //devolver un alerta que diga (Hola (nombre de usuario/a) bienvenid@ a la pagina);
+ // otro alerta que diga el resultado de los dos numeros ingresados es (resultado); 
+ // escribe en pantalla (document.write) "el resultado de la suma de tus numeros mas la cantidad de las letras de tu nombre es: (resultado de la suma + las letras de su nombre)"
+
+
+
+
+
