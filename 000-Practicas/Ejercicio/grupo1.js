c4f025e020088a083ae1d67ef609eefc7df26721
BeerCarolina <carolina.beer@arbusta.net>
2022-03-21 13:08:32 -0300

    Ejercicio Grupo1

--- 000-Practicas/Ejercicio/grupo1.js
+++ 000-Practicas/Ejercicio/grupo1.js
@@ -1,1 +1,15 @@
+    let nombre = prompt ("Ingrese su nombre");
+    let numero1 = parseInt (prompt ("Ingrese un número")); //parseInt toma como numero el valor que se ingresa, siempre que sea entero//
     
+    
+    alert ("Hola "+ nombre + " bienvenid@ a la pagina");
+
+    let numero2 = parseInt (prompt ("Ingrese otro número"));
+
+    let resultado = (numero1 + numero2);
+
+    alert ("El resultado de los dos numeros ingresados es " + resultado);
+
+    document.write ( "el resultado de la suma de tus numeros mas la cantidad de las letras de tu nombre es: " + parseInt (nombre.length + resultado));
+
+
