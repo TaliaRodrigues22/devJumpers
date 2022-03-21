d682a10c5a7adf226aa5597a0d4552be89245248
SebastianAvila00 <sebastian.avila@arbusta.net>
2022-03-21 14:54:33 -0300

    Grupo 4

---
+++ 000-Practicas/Ejercicio/Grupo4.js
@@ -0,0 +1,13 @@
+
+let nombre = prompt ("Hola ¿Cómo te llamas?");
+alert("Hola " + nombre + " Bienvenidx a la página");
+
+let numero1 = parseInt (prompt ("Ingrese un número"));
+
+let numero2 = parseInt (prompt ("Ingrese otro número"));
+
+let resultado = (numero1 + numero2);
+alert ("El resultado de los dos numeros ingresados es " + resultado);
+
+
+document.write("El resultado de las sumas mas la cantidad de la letra de tu nombre es: " + (nombre.length + resultado));
