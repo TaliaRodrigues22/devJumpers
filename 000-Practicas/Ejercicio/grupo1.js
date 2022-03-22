0e654a323a06dd45e4b82e2359c3c898e2fb50be
BeerCarolina <carolina.beer@arbusta.net>
2022-03-22 16:04:18 -0300

    ejercicio 2 grupo 1

--- 000-Practicas/Ejercicio/grupo1.js
+++ 000-Practicas/Ejercicio/grupo1.js
@@ -1,15 +1,52 @@
-    let nombre = prompt ("Ingrese su nombre");
-    let numero1 = parseInt (prompt ("Ingrese un número")); //parseInt toma como numero el valor que se ingresa, siempre que sea entero//
-    
-    
-    alert ("Hola "+ nombre + " bienvenid@ a la pagina");
+    /* 
 
-    let numero2 = parseInt (prompt ("Ingrese otro número"));
+let nombre = prompt("Ingrese su nombre");
+let numero1 = parseInt(prompt("Ingrese un número")); //parseInt toma como numero el valor que se ingresa, siempre que sea entero//
 
-    let resultado = (numero1 + numero2);
 
-    alert ("El resultado de los dos numeros ingresados es " + resultado);
+alert("Hola " + nombre + " bienvenid@ a la pagina");
 
-    document.write ( "el resultado de la suma de tus numeros mas la cantidad de las letras de tu nombre es: " + parseInt (nombre.length + resultado));
+let numero2 = parseInt(prompt("Ingrese otro número"));
+
+let resultado = (numero1 + numero2);
+
+alert("El resultado de los dos numeros ingresados es " + resultado);
+
+document.write("el resultado de la suma de tus numeros mas la cantidad de las letras de tu nombre es: " + parseInt(nombre.length + resultado) ); 
+*/
+
+
+
+
+
+//1.- Escribe un programa de una sola línea que haga que aparezca en la pantalla un alert que diga «Hello Arbi».
+
+alert("Hello Arbi");
+//2.- Escribe un programa de una sola línea que escriba en la pantalla un texto que diga «Hello Arbi».
+document.write ("Hello Arbi<br/>")
+//3.- Escribe un programa de una sola línea que escriba en la pantalla el resultado de sumar 3 + 5.
+document.write ("El resultado de 3 + 5 =" + (3 + 5) +"<br/>");
+//4.- Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba en pantalla un texto que diga «Hola (AcavaNombreQueIngresoElUsuarix)»
+let nombre = prompt("Ingrese su nombre");
+document.write (`Hola ${nombre} <br/>`);
+
+//5.- Escribe un programa de dos lineas que pida el nombre al usuario con un prompt y escriba en pantalla un texto que diga «Hola (nombre) tu nombre tiene (cantidaddeletras) letras»
+
+document.write (`Hola ${nombre} tu nombre tiene ${nombre.length}`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
