6395563209f4898acc9b9f61d602c8429ee57483
Arbusta <iara.gerardi@arbusta.net>
2022-03-22 14:52:58 -0300

    agrego el ejercicio nuevo en el archivo del grupo 6

--- 000-Practicas/Ejercicio/grupo6.js
+++ 000-Practicas/Ejercicio/grupo6.js
@@ -1,3 +1,5 @@
+ //Ejercicio uno
+ 
  let nombre = prompt("¿Cual es tu nombre?");
 
  let numero1 = parseInt(prompt("ingresa un numero"));
@@ -10,3 +12,17 @@
 
  let total = suma + (nombre.length);
  document.write ("El resultado total es: " + total); 
+
+//Ejercicio 2
+
+alert('hello arbi')
+
+document.write (" hello arbi ")
+
+document.write (3+5)
+
+let nombreDos = prompt("ingrese nombre de usuario ")
+document.write (" Hola " + nombreDos)
+
+let nombre2 = prompt("ingresa tu nombre de nuevo")
+document.write(" Hola " + nombreDos + " tu nombre tiene " + nombreDos.length + " letras")
