5b66626592f1aa7bd0b3cd9028c82733565ebf77
SofiaSerrano2 <sofia.serrano@arbusta.net>
2022-03-22 15:14:34 -0300

    Grupo 4 ejercicio 2

--- 000-Practicas/Ejercicio/Grupo4.js
+++ 000-Practicas/Ejercicio/Grupo4.js
@@ -1,13 +1,44 @@
 
-let nombre = prompt ("Hola ¿Cómo te llamas?");
-alert("Hola " + nombre + " Bienvenidx a la página");
+// let nombre = prompt ("Hola ¿Cómo te llamas?");
+// alert("Hola " + nombre + " Bienvenidx a la página");
 
-let numero1 = parseInt (prompt ("Ingrese un número"));
+// let numero1 = parseInt (prompt ("Ingrese un número"));
 
-let numero2 = parseInt (prompt ("Ingrese otro número"));
+// let numero2 = parseInt (prompt ("Ingrese otro número"));
 
-let resultado = (numero1 + numero2);
-alert ("El resultado de los dos numeros ingresados es " + resultado);
+// let resultado = (numero1 + numero2);
+// alert ("El resultado de los dos numeros ingresados es " + resultado);
 
 
-document.write("El resultado de las sumas mas la cantidad de la letra de tu nombre es: " + (nombre.length + resultado));
+// document.write("El resultado de las sumas mas la cantidad de la letra de tu nombre es: " + (nombre.length + resultado));
+
+
+// let nombre = prompt ("Hola ¿Cómo te llamas?");
+// alert("Hola " + nombre + " Bienvenidx a la página");
+
+// let numero1 = parseInt (prompt ("Ingrese un número"));
+
+// let numero2 = parseInt (prompt ("Ingrese otro número"));
+
+// let resultado = (numero1 + numero2);
+// alert ("El resultado de los dos numeros ingresados es " + resultado);
+
+// document.write("El resultado de las sumas mas la cantidad de la letra de tu nombre es: " + (nombre.length + resultado));
+
+// Les dejo ejercicios complementarios para los grupos que terminaron . (Agregarlos en su archivo js )
+//1.- Escribe un programa de una sola línea que haga que aparezca en la pantalla un alert que diga «Hello Arbi».
+//2.- Escribe un programa de una sola línea que escriba en la pantalla un texto que diga «Hello Arbi».
+//3.- Escribe un programa de una sola línea que escriba en la pantalla el resultado de sumar 3 + 5.
+//4.- Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba en pantalla un texto que diga «Hola (AcavaNombreQueIngresoElUsuarix)»
+//5.- Escribe un programa de dos lineas que pida el nombre al usuario con un prompt y escriba en pantalla un texto que diga «Hola (nombre) tu nombre tiene (cantidaddeletras) letras»
+
+/**Ejercicio 1*/
+alert("Hola Arbi");
+/**Ejercicio 2*/
+document.write("<br/>Hola Arbi");
+/*Ejerciocio 3*/
+document.write("<br/> La suma entre 3 + 5 es: "+(3+5));
+//Ejercicio 4/5.- Escribe un programa de dos lineas que pida el nombre al usuario con un prompt y 
+//escriba en pantalla un texto que diga «Hola (nombre) tu nombre tiene (cantidaddeletras) letras»
+let nombre = prompt("Ingrese nombre");
+document.write("<br/>Hola "+nombre + " tu nombre tiene " +nombre.length +" letras");
