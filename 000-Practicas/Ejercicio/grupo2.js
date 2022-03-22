099281fcab4755d73ec5751cb1e3856718003535
Guada Garcilazo <guadalupe.garcilazo@arbusta.net>
2022-03-22 15:55:20 -0300

    Grupo 2 ejercicios

--- 000-Practicas/Ejercicio/grupo2.js
+++ 000-Practicas/Ejercicio/grupo2.js
@@ -1,4 +1,5 @@
-let nombre = prompt ("Escribe tu nombre");
+
+/* let nombre = prompt ("Escribe tu nombre");
 
 alert ("Hola " + nombre + " bienvenido/a a la página")
 
@@ -10,4 +11,37 @@ let resultado  = (number1 + number2);
 
 alert ("El resultado de los dos numeros ingresados es " + resultado );
 
-document.write ("El resultado de la suma de tus numeros mas la cantidad de las letras de tu nombre es " + (nombre.length + resultado))
+document.write ("El resultado de la suma de tus numeros mas la cantidad de las letras de tu nombre es " + (nombre.length + resultado)) ; */
+
+
+//1.- Escribe un programa de una sola línea que haga que aparezca en la pantalla un alert que diga «Hello Arbi».
+
+//2.- Escribe un programa de una sola línea que escriba en la pantalla un texto que diga «Hello Arbi».
+
+//3.- Escribe un programa de una sola línea que escriba en la pantalla el resultado de sumar 3 + 5.
+
+//4.- Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba en pantalla un texto que diga «Hola (AcavaNombreQueIngresoElUsuarix)»
+
+//5.- Escribe un programa de dos lineas que pida el nombre al usuario con un prompt y escriba en pantalla un texto que diga «Hola (nombre) tu nombre tiene (cantidaddeletras) letras»
+
+
+// 1,2,3 ejercicios
+
+/* alert ("Hello Arbi") ;  document.write (" Hello Arbi") ; let suma = (3 + 5) ; document.write ( "   " + suma ) ; */
+
+/* let nombre = (prompt ("Hola")) ; 
+
+document.write ("Hola " + nombre) ; */
+
+// 4,5 ejercicios
+
+let nombre = (prompt ("Hola, escribe tu nombre de usuario")) ;
+
+document.write ("Hola " + nombre + " tu nombre tiene " + nombre.length + " letras") ;
+
+
+
+
+
+
+
