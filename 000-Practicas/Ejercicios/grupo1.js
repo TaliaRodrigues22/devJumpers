73aa13cf1c673664028ac02e118036ff25e9af07
camiAguirre4rb1 <camila.aguirre@arbusta.net>
2022-03-23 17:05:48 -0300

    cambio 3 grupo 1

--- 000-Practicas/Ejercicios/grupo1.js
+++ 000-Practicas/Ejercicios/grupo1.js
@@ -52,24 +52,33 @@ document.write("el resultado de la suma de tus numeros mas la cantidad de las le
 
 //} else{
    //  alert(`${nombre} no ingresaste ningun numero`)// si el usuari/o ingresa una letra devolver un mensaje de error
-//}
+//};
 
 
 
                                                            //Ejercicio 2
 // Crear un programa que:
 // pida el nombre
-let nombre= prompt("Ingresá tu nombre");
+//let nombre= prompt("Ingresá tu nombre");
 // mostrar un alerta que le diga la cantidad de letras que tiene su nombre
-alert (`Tu nombre tiene ${nombre.length} letras `);
+//alert (`Tu nombre tiene ${nombre.length} letras `);
 // que pida un número
-let numero=parseInt (prompt("Ingresá un número"));
+//let numero=parseInt (prompt("Ingresá un número"));
 // Mostrar un alerta que le muestre al usuario/a el resultado de la suma del numero ingresado + la cantidad de caracteres de su nombre.
-let resultado= (nombre.length + numero)
-alert (`La suma de las letras de tu nombre más el número que ingresaste es ${resultado}`)
+//let resultado= (nombre.length + numero);
+//alert (`La suma de las letras de tu nombre más el número que ingresaste es ${resultado}`);
 
 // Ejercicio 3
 //Escribe un programa que pida dos números y escriba en la pantalla cual es el mayor.
+let number1 = parseInt(prompt("ingresa un número"));
+
+let number2 = parseInt(prompt("ingresa otro número"));
+
+if(number1>number2){
+    document.write(`El ${number1} es el número mas grande que ingresaste`)
+}else{
+    document.write(`El ${number2} es el número mas grande que ingresaste `)
+};
 
 /* Ejercicio 4: Condicionales
 1) Una pequeña despensa desea calcular los sueldos de sus empleados. Los puestos
