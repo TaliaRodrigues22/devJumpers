a70c3ef00c42c09ff1264efbba2c64e29b541b27
Arbusta <gabriel.munoz@arbusta.net>
2022-03-25 11:18:02 -0300

    Ejercicios 25-3

---
+++ 000-Practicas/Ejercicios/25-03/Ejercicios.js
@@ -0,0 +1,10 @@
+// Ejercicio 1: Escribe un  programa que pida 3 números y escriba en la pantalla el mayor de los tres.
+
+
+
+
+//Ejercicio 2: Escribe un programa que pida un número y diga si es divisible por 2
+
+
+
+//Ejercicio 3: Escribe un programa que pida un número y nos diga si es divisible por 2, 3, 5 o 7 (sólo hay que comprobar si lo es por uno de los cuatro) */
