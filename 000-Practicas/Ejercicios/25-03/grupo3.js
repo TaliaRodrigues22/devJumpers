45e4c1e33ac50aea756ec2cc7836b725a3ac0d26
Cristian Vera <cristian.vera@arbusta.net>
2022-03-25 17:51:55 -0300

    Trabajo Grupo 3

---
+++ 000-Practicas/Ejercicios/25-03/grupo3.js
@@ -0,0 +1,44 @@
+// // let numero = parseInt(prompt("INTROIDUCE UN NUMERO"))
+
+// let num1 = parseInt(prompt("ESCRIBE UN NUMERO"))
+// let num2 = parseInt(prompt("ESCRIBE UN NUMERO"))
+// let num3 = parseInt(prompt("ESCRIBE UN NUMERO"))
+// let mayor 
+
+// if (num1 > num2 && num1 > num3){
+//  mayor = num1;
+// }
+// else if (num2 > num1 && num2> num3){
+//  mayor = num2
+// }
+// else{mayor = num3;
+// }
+// document.write("EL NUMERO MAYOR ES" + mayor);
+
+// //--------------------------------------------------------------------------------
+
+// //Ejercicio 2: Escribe un programa que pida un número y diga si es divisible por 2
+
+
+// let numero = parseInt(prompt("Escribe un numero"))
+
+// if (numero % 2 === 0 ){
+//     document.write("es divisible por dos");
+
+// } else {
+//     document.write(" no es divisible por 2")
+// }
+
+// //----------------------------------------------------------------------------------
+
+// //Ejercicio 3: Escribe un programa que pida un número y nos diga si es divisible por 2, 3, 5 o 7 (sólo hay que comprobar si lo es por uno de los cuatro) */
+
+// let numero = parseInt(prompt ("ingrese un numero"));
+
+
+// if (numero % 2 === 0 || numero % 3 === 0 || numero % 5 === 0 || numero % 7 === 0 ) {
+//     alert(`El número ingresado ${numero} es divisible por 2, por 3, por 5 o por 7`);
+// }else {
+//     alert(`El número ingresado ${numero} no es divisible por 2, por 3, por 5 ni por 7`);
+// }
+
